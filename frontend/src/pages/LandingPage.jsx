a7e2ca62880d029b6df89ab586d6226710c75a32
Nilanjan Pramanik <nik.nilanjan@gmail.com>
2024-03-30 21:35:06 +0530

    2

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -3,7 +3,24 @@ import './style.css'
 
 const LandingPage = () => {
   return (
-    <div>This is a landing page</div>
+    <div class="box">
+        
+      <div class="card">
+      <p class="card-title">Rise and Thrive:Empowering Women for a Brighter Tomorrow</p>
+      <p class="small-desc">
+        Empowering women by giving them an exposure to the world.Uplifting their ideas,
+        breaking barriers and fostering 
+        a supportive community where women can thrive,express themselves and build
+         a brighter future for themselves and their families.
+      </p>
+      <div class="go-corner">
+        <div class="go-arrow">→</div>
+      </div>
+       </div>
+        <div class="Slider">
+        <div class="imgSlider"></div>
+     </div>
+     </div>
   )
 }
 
